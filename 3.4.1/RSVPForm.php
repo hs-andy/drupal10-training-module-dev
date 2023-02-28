bac9fce826aa944110cd6c8309c64a5ba1da7512
hs-andy <h16001am@manyhappyseed.com>
2023-02-28 22:20:59 +0530

    Module Dev exercise

---
+++ 3.4.1/RSVPForm.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\rsvplist\Form\RSVPForm
+ */
+namespace Drupal\rsvplist\Form;
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+
+/**
+ * Provides a RSVP Email form.
+ */
+class RSVPForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'rsvplist_email_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $nid = $node ? $node->nid->value : false;
+    $form['email'] = array(
+      '#title' => t('Email address'),
+      '#type' => 'textfield',
+      '#size' => 25,
+      '#description' => t("We'll send updates to the email address you provide."),
+      '#required' => TRUE,
+    );
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('RSVP'),
+    );
+    $form['nid'] = array(
+      '#type' => 'hidden',
+      '#value' => $nid,
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $value = $form_state->getValue('email');
+    if ($value == !\Drupal::service('email.validator')->isValid($value)) {
+      $form_state->setErrorByName('email', t('The email address %mail is not valid.', array('%mail' => $value)));
+      return;
+    }
+  }
+  
+ /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $user = \Drupal::currentUser();
+    $query = \Drupal::database()->insert('rsvplist');
+    $query->fields([
+      'mail',
+      'nid',
+      'uid',
+      'created',
+    ]);
+    $query->values([
+      $form_state->getValue('email'),
+      $form_state->getValue('nid'),
+      $user->id(),
+      time(),
+    ]);
+    $query->execute();
+    $this->messenger()->addMessage(t('Thank you for your RSVP, you are on the list for the event.'));
+  }
+}
